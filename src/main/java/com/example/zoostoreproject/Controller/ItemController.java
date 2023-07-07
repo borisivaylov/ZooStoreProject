09342a5d4a50c87ae0df6e934fe3adb8a966b81e
borisivaylov <boris_ivaylov@abv.bg>
2023-07-07 11:22:09 +0300

    Initial commit

---
+++ src/main/java/com/example/zoostoreproject/Controller/ItemController.java
@@ -0,0 +1,4 @@
+package com.example.zoostoreproject.Controller;
+
+public class ItemController {
+}
---
+++ src/test/java/com/example/zoostoreproject/ZooStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoostoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooStoreProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
