69723d5dc6a2aedf22677001e74c9c99edf15d25
borisivaylov <boris_ivaylov@abv.bg>
2023-07-12 23:12:15 +0300

    Project structure refactoring

--- src/main/java/com/example/zoostoreproject/Bussiness/operations/vendor/update/VendorUpdateService.java
+++ src/main/java/com/example/zoostoreproject/Bussiness/operations/vendor/update/VendorUpdateService.java
@@ -1,4 +1,40 @@
 package com.example.zoostoreproject.Bussiness.operations.vendor.update;
 
+import com.example.zoostoreproject.API.Operations.Vendor.VendorRequest;
+import com.example.zoostoreproject.API.Operations.Vendor.VendorResponse;
+import com.example.zoostoreproject.Bussiness.Mappers.Vendor.VendorMapperImpl;
+import com.example.zoostoreproject.Data.Entity.Vendor;
+import com.example.zoostoreproject.Data.Repository.VendorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+
 public class VendorUpdateService {
+
+    private VendorRepository vendorRepository;
+    public VendorUpdateService(VendorRepository vendorRepository) {
+        this.vendorRepository = vendorRepository;
+    }
+
+    public Vendor updateVendor(UUID id, String newVendorName){
+
+        Vendor vendor = vendorRepository.findById(id).orElseThrow(()->
+              new IllegalArgumentException("Vendor not found with ID: " + id))  ;
+                vendor.setVendorName(newVendorName);
+
+       // VendorRequest vendorRequest = VendorRequest.builder()
+       //         .id(id).build();
+
+       // Optional<Vendor> optionalVendor= vendorRepository.findById(vendorRequest.getId());
+
+      //  Vendor vendor = optionalVendor.get();
+      //  vendor.setVendorName(newVendorName);
+
+        return vendorRepository.save(vendor);
+    }
 }
--- src/main/java/com/example/zoostoreproject/Controller/VendorController.java
+++ src/main/java/com/example/zoostoreproject/Controller/VendorController.java
@@ -1,13 +1,14 @@
 package com.example.zoostoreproject.Controller;
 
+import com.example.zoostoreproject.Bussiness.operations.vendor.create.VendorCreateService;
+import com.example.zoostoreproject.Bussiness.operations.vendor.update.VendorUpdateService;
 import com.example.zoostoreproject.Data.Entity.Vendor;
 import com.example.zoostoreproject.API.Operations.Vendor.VendorRequest;
 import com.example.zoostoreproject.Data.Repository.VendorRepository;
 import com.example.zoostoreproject.Bussiness.operations.vendor.VendorServiceImpl;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/vendor")
@@ -15,16 +16,22 @@ public class VendorController {
 
 
     private final VendorRepository vendorRepository;
-    VendorServiceImpl vendorService;
+    private final VendorCreateService vendorCreateService;
+    private  final VendorUpdateService vendorUpdateService;
 
-    public VendorController(VendorRepository vendorRepository, VendorServiceImpl vendorService) {
+    public VendorController(VendorRepository vendorRepository, VendorCreateService vendorCreateService, VendorUpdateService vendorUpdateService) {
         this.vendorRepository = vendorRepository;
-        this.vendorService = vendorService;
+        this.vendorCreateService = vendorCreateService;
+        this.vendorUpdateService = vendorUpdateService;
     }
 
     @PostMapping("/addVendor")
     Vendor newVendor(@RequestBody VendorRequest vendorRequest){
-        return vendorRepository.save(vendorService.newVendor(vendorRequest));
+        return vendorRepository.save(vendorCreateService.newVendor(vendorRequest));
+    }
+    @PutMapping ("/updateVendor")
+    Vendor updateVendor(@RequestBody UUID id, @RequestBody String newVendorName){
+        return vendorRepository.save(vendorUpdateService.updateVendor(id,newVendorName));
     }
 
 }
